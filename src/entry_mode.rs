e6ef47f9855b964a27f4a79611f8d589c70a2f7f
Hendrik Kunert <hendrik.kunert@henku.de>
2017-07-01 01:36:34 +0200

    Refactor entry mode builder

--- src/entry_mode.rs
+++ src/entry_mode.rs
@@ -2,13 +2,14 @@ bitflags! {
     struct EntryModeFlags: u8 {
         const ENTRY_MODE            = 0b00000100;
         const CURSOR_MOVE_INCREMENT = 0b00000010;
-        const DISPLAY_SHIFT_ENABLE  = 0b00000001;
+        const DISPLAY_SHIFT_ON      = 0b00000001;
         const CURSOR_MOVE_DECREMENT = 0b00000000;
-        const DISPLAY_SHIFT_DISABLE = 0b00000000;
+        const DISPLAY_SHIFT_OFF     = 0b00000000;
     }
 }
 
 /// Enumeration of possible methods to move.
+#[derive(Clone, Copy)]
 pub enum MoveDirection {
     /// Moves right.
     Increment,
@@ -16,10 +17,35 @@ pub enum MoveDirection {
     Decrement,
 }
 
+impl From<MoveDirection> for EntryModeFlags {
+    fn from(direction: MoveDirection) -> Self {
+        match direction {
+            MoveDirection::Increment => CURSOR_MOVE_INCREMENT,
+            MoveDirection::Decrement => CURSOR_MOVE_DECREMENT,
+        }
+    }
+}
+
+/// Enumeration to set display shift.
+#[derive(Clone, Copy)]
+pub enum DisplayShift {
+    On,
+    Off
+}
+
+impl From<DisplayShift> for EntryModeFlags {
+    fn from(shift: DisplayShift) -> Self {
+        match shift {
+            DisplayShift::On => DISPLAY_SHIFT_ON,
+            DisplayShift::Off => DISPLAY_SHIFT_OFF,
+        }
+    }
+}
+
 /// A struct for creating display entry mode settings.
 pub struct EntryModeBuilder {
     move_direction: MoveDirection,
-    display_shift: bool,
+    display_shift: DisplayShift,
 }
 
 impl EntryModeBuilder {
@@ -34,7 +60,7 @@ impl EntryModeBuilder {
     pub fn new() -> EntryModeBuilder {
         EntryModeBuilder {
             move_direction: MoveDirection::Increment,
-            display_shift: false,
+            display_shift: DisplayShift::Off,
         }
     }
 
@@ -45,32 +71,24 @@ impl EntryModeBuilder {
         self
     }
 
-    /// Sets the display shift, which will be on character write, either `On` or `Off`.
+    /// Sets the display shift, which will be performed on character write, either `On` or `Off`.
     ///
     /// If display shift is enabled, it will seem as if the cursor does not move but the display
     /// does.
     ///
     /// **Note:** The display does not shift when reading.
-    pub fn set_display_shift(&mut self, shift: bool) -> &mut EntryModeBuilder {
+    pub fn set_display_shift(&mut self, shift: DisplayShift) -> &mut EntryModeBuilder {
         self.display_shift = shift;
         self
     }
 
     pub fn build_command(&self) -> u8 {
-        let mut cmd = ENTRY_MODE.bits();
+        let mut cmd = ENTRY_MODE;
 
-        cmd |= match self.move_direction {
-            MoveDirection::Increment => CURSOR_MOVE_INCREMENT.bits(),
-            MoveDirection::Decrement => CURSOR_MOVE_DECREMENT.bits(),
-        };
+        cmd |= EntryModeFlags::from(self.move_direction);
+        cmd |= EntryModeFlags::from(self.display_shift);
 
-        cmd |= if self.display_shift {
-            DISPLAY_SHIFT_ENABLE.bits()
-        } else {
-            DISPLAY_SHIFT_DISABLE.bits()
-        };
-
-        cmd
+        cmd.bits()
     }
 }
 
