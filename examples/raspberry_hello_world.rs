f4d66f145c088d87754f094dcc94318ba2ca8a66
Hendrik Kunert <hendrik.kunert@henku.de>
2017-07-17 21:27:03 +0200

    Refactor examples

--- examples/raspberry_hello_world.rs
+++ examples/raspberry_hello_world.rs
@@ -1,37 +1,49 @@
 extern crate sysfs_gpio;
 extern crate clerk;
 
-use clerk::{SeekFrom, Display, DisplayPins, DisplayHardwareLayer, ShiftTo, DefaultLines,
-            DisplayState, CursorState, CursorBlinking};
+use clerk::{SeekFrom, Display, DisplayPins, DisplayHardwareLayer, DefaultLines, DisplayState,
+            CursorState, CursorBlinking, Direction};
 
-use sysfs_gpio::{Direction, Pin};
-
-struct ExternPin(Pin);
+struct ExternPin(sysfs_gpio::Pin);
 
 impl From<u64> for ExternPin {
     fn from(i: u64) -> Self {
-        ExternPin(Pin::new(i))
+        ExternPin(sysfs_gpio::Pin::new(i))
     }
 }
 
 impl DisplayHardwareLayer for ExternPin {
     fn init(&self) {
         self.0.export().unwrap();
-        self.0.set_direction(Direction::Out).unwrap();
+        self.0.set_direction(sysfs_gpio::Direction::Out).unwrap();
     }
 
     fn cleanup(&self) {
         self.0.unexport().unwrap();
     }
 
+    fn set_direction(&self, direction: Direction) {
+        let native_direction = match direction {
+            Direction::In => sysfs_gpio::Direction::In,
+            Direction::Out => sysfs_gpio::Direction::Out,
+        };
+
+        self.0.set_direction(native_direction).unwrap();
+    }
+
     fn set_value(&self, value: u8) -> Result<(), ()> {
         self.0.set_value(value).map_err(|_| ())
+    }
+
+    fn get_value(&self) -> u8 {
+        self.0.get_value().unwrap()
     }
 }
 
 fn main() {
     let pins = DisplayPins {
         register_select: 2,
+        read: 3,
         enable: 4,
         data4: 16,
         data5: 19,
@@ -47,23 +59,12 @@ fn main() {
             .set_cursor_blinking(CursorBlinking::On);
     });
 
-
-    lcd.seek(SeekFrom::Line {
-        line: DefaultLines::One,
-        bytes: 0,
-    });
-    lcd.shift_cursor(ShiftTo::Right(2));
-    lcd.write_message("Hallo");
+    lcd.write_message("Hello");
 
     lcd.seek(SeekFrom::Line {
         line: DefaultLines::Two,
-        bytes: 0,
+        bytes: 5,
     });
-    lcd.shift_cursor(ShiftTo::Right(2));
-    lcd.write_message("du");
-    lcd.shift_cursor(ShiftTo::Left(2));
 
-    lcd.shift(ShiftTo::Right(4));
-
-    lcd.seek(SeekFrom::Current(5));
+    lcd.write_message("world!");
 }
---
+++ examples/raspberry_read.rs
@@ -0,0 +1,79 @@
+extern crate sysfs_gpio;
+extern crate clerk;
+
+use clerk::{SeekFrom, Display, DisplayPins, DisplayHardwareLayer, DefaultLines, DisplayState,
+            CursorState, CursorBlinking, Direction};
+
+struct ExternPin(sysfs_gpio::Pin);
+
+impl From<u64> for ExternPin {
+    fn from(i: u64) -> Self {
+        ExternPin(sysfs_gpio::Pin::new(i))
+    }
+}
+
+impl DisplayHardwareLayer for ExternPin {
+    fn init(&self) {
+        self.0.export().unwrap();
+        self.0.set_direction(sysfs_gpio::Direction::Out).unwrap();
+    }
+
+    fn cleanup(&self) {
+        self.0.unexport().unwrap();
+    }
+
+    fn set_direction(&self, direction: Direction) {
+        let native_direction = match direction {
+            Direction::In => sysfs_gpio::Direction::In,
+            Direction::Out => sysfs_gpio::Direction::Out,
+        };
+
+        self.0.set_direction(native_direction).unwrap();
+    }
+
+    fn set_value(&self, value: u8) -> Result<(), ()> {
+        self.0.set_value(value).map_err(|_| ())
+    }
+
+    fn get_value(&self) -> u8 {
+        self.0.get_value().unwrap()
+    }
+}
+
+fn main() {
+    let pins = DisplayPins {
+        register_select: 2,
+        read: 3,
+        enable: 4,
+        data4: 16,
+        data5: 19,
+        data6: 26,
+        data7: 20,
+    };
+
+    let mut lcd: Display<ExternPin, DefaultLines> = Display::from_pins(pins);
+
+    lcd.set_display_control(|e| {
+        e.set_display(DisplayState::On)
+            .set_cursor(CursorState::Off)
+            .set_cursor_blinking(CursorBlinking::On);
+    });
+
+    lcd.write_message("Hello");
+
+    lcd.seek(SeekFrom::Home(0));
+    let value = lcd.read_byte();
+    println!("Value is: {}", value as char);
+
+    let value = lcd.read_byte();
+    println!("Value is: {}", value as char);
+
+    let (busy_flag, address) = lcd.read_busy_flag();
+    println!("Busy Flag: {}, Address: {}", busy_flag, address);
+
+    let (busy_flag, address) = lcd.read_busy_flag();
+    println!("Busy Flag: {}, Address: {}", busy_flag, address);
+
+    lcd.seek(SeekFrom::Current(0));
+    lcd.write_message("llo World!");
+}
