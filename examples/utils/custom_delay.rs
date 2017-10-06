d9c15766f0e7d012600543bdf697fa668551ebb8
Hendrik Kunert <hendrik.kunert@henku.de>
2017-10-06 11:58:16 +0200

    Fix clippy issues

--- examples/utils/custom_delay.rs
+++ examples/utils/custom_delay.rs
@@ -6,6 +6,6 @@ pub struct CustomDelay;
 
 impl Delay for CustomDelay {
     fn delay_ns(ns: u16) {
-        thread::sleep(time::Duration::new(0, ns as u32));
+        thread::sleep(time::Duration::new(0, u32::from(ns)));
     }
 }
