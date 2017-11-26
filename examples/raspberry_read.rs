aaf58002faf873de0cc67db6cbd8be269bcad334
Hendrik Kunert <qkunerd@gmail.com>
2017-11-26 21:09:59 +0100

    Refactor read

--- examples/raspberry_read.rs
+++ examples/raspberry_read.rs
@@ -1,50 +1,52 @@
-// extern crate clerk;
-// extern crate sysfs_gpio;
+extern crate clerk;
+extern crate sysfs_gpio;
 
-// use clerk::{CursorBlinking, CursorState, DefaultLines, Display, DisplayControlBuilder,
-//             DisplayPins, DisplayState, FunctionSetBuilder, SeekFrom};
+use clerk::{CursorBlinking, CursorState, DataPins4Lines, DefaultLines, Display,
+            DisplayControlBuilder, DisplayState, FunctionSetBuilder, Pins, SeekFrom};
 
-// mod utils;
-// use utils::ExternPin;
-// use utils::CustomDelay;
+mod utils;
+use utils::ExternPin;
+use utils::CustomDelay;
 
-// fn main() {
-//     let pins = DisplayPins {
-//         register_select: ExternPin::new(2),
-//         read: ExternPin::new(3),
-//         enable: ExternPin::new(4),
-//         data4: ExternPin::new(16),
-//         data5: ExternPin::new(19),
-//         data6: ExternPin::new(26),
-//         data7: ExternPin::new(20),
-//     };
+fn main() {
+    let pins = Pins {
+        register_select: ExternPin::new(2),
+        read: ExternPin::new(3),
+        enable: ExternPin::new(4),
+        data: DataPins4Lines {
+            data4: ExternPin::new(16),
+            data5: ExternPin::new(19),
+            data6: ExternPin::new(26),
+            data7: ExternPin::new(20),
+        },
+    };
 
-//     let mut lcd: Display<ExternPin, DefaultLines, CustomDelay> = Display::from_pins(pins);
+    let mut lcd: Display<_, DefaultLines> = Display::from_pins(pins);
 
-//     lcd.init(&FunctionSetBuilder::default());
+    lcd.init(&FunctionSetBuilder::default());
 
-//     lcd.set_display_control(
-//         DisplayControlBuilder::default()
-//             .set_display(DisplayState::On)
-//             .set_cursor(CursorState::Off)
-//             .set_cursor_blinking(CursorBlinking::On),
-//     );
+    lcd.set_display_control(
+        DisplayControlBuilder::default()
+            .set_display(DisplayState::On)
+            .set_cursor(CursorState::Off)
+            .set_cursor_blinking(CursorBlinking::On),
+    );
 
-//     lcd.write_message("Hello");
+    lcd.write_message("Hello");
 
-//     lcd.seek(SeekFrom::Home(0));
-//     let value = lcd.read_byte();
-//     println!("Value is: {}", value as char);
+    lcd.seek(SeekFrom::Home(0));
+    let value = lcd.read_byte();
+    println!("Value is: {}", value as char);
 
-//     let value = lcd.read_byte();
-//     println!("Value is: {}", value as char);
+    let (busy_flag, address) = lcd.read_busy_flag();
+    println!("Busy Flag: {}, Address: {}", busy_flag, address);
 
-//     let (busy_flag, address) = lcd.read_busy_flag();
-//     println!("Busy Flag: {}, Address: {}", busy_flag, address);
+    let value = lcd.read_byte();
+    println!("Value is: {}", value as char);
 
-//     let (busy_flag, address) = lcd.read_busy_flag();
-//     println!("Busy Flag: {}, Address: {}", busy_flag, address);
+    let (busy_flag, address) = lcd.read_busy_flag();
+    println!("Busy Flag: {}, Address: {}", busy_flag, address);
 
-//     lcd.seek(SeekFrom::Current(0));
-//     lcd.write_message("llo World!");
-// }
+    lcd.seek(SeekFrom::Current(0));
+    lcd.write_message("llo World!");
+}
--- src/display.rs
+++ src/display.rs
@@ -32,7 +32,7 @@ pub enum WriteMode {
     Data(u8),
 }
 
-enum ReadMode {
+pub enum ReadMode {
     Data,
     // TODO: use busy flag
     BusyFlag,
@@ -155,12 +155,20 @@ pub trait DisplayHardwareLayer {
     fn get_value(&self) -> u8;
 }
 
+pub trait Send {
+    fn send(&self, mode: WriteMode);
+}
+
 pub trait SendRaw {
     fn send_byte(&self, byte: u8);
 }
 
-pub trait Send {
-    fn send(&self, mode: WriteMode);
+pub trait Recieve {
+    fn recieve(&self, mode: ReadMode) -> u8;
+}
+
+pub trait RecieveRaw {
+    fn receive_byte(&self) -> u8;
 }
 
 pub trait Init {
@@ -210,6 +218,25 @@ where
         self.send_byte(value);
     }
 }
+
+impl<RS, R, E, D> Recieve for Pins<RS, R, E, D>
+where
+    Self: RecieveRaw,
+    RS: DisplayHardwareLayer,
+    R: DisplayHardwareLayer,
+{
+    fn recieve(&self, mode: ReadMode) -> u8 {
+        self.read.set_level(Level::High);
+
+        match mode {
+            ReadMode::Data => self.register_select.set_level(Level::High),
+            ReadMode::BusyFlag => self.register_select.set_level(Level::Low),
+        };
+
+        self.receive_byte()
+    }
+}
+
 
 fn get_bit(val: u8, bit: u8) -> Level {
     if val & bit == bit {
@@ -390,6 +417,57 @@ fn write_4bit<RS, R, E, P4, P5, P6, P7>(
     // D::delay_ns(D::DATA_HOLD_TIME);
 }
 
+impl<RS, R, E, P4, P5, P6, P7> RecieveRaw for Pins<RS, R, E, DataPins4Lines<P4, P5, P6, P7>>
+where
+    E: DisplayHardwareLayer,
+    P4: DisplayHardwareLayer,
+    P5: DisplayHardwareLayer,
+    P6: DisplayHardwareLayer,
+    P7: DisplayHardwareLayer,
+{
+    fn receive_byte(&self) -> u8 {
+        self.data.data4.set_direction(Direction::In);
+        self.data.data5.set_direction(Direction::In);
+        self.data.data6.set_direction(Direction::In);
+        self.data.data7.set_direction(Direction::In);
+
+        let upper = read_single_nibble(self);
+        let lower = read_single_nibble(self);
+
+        let mut result = upper << 4;
+        result |= lower & 0x0f;
+
+        result
+    }
+}
+
+fn read_single_nibble<RS, R, E, P4, P5, P6, P7>(
+    pins: &Pins<RS, R, E, DataPins4Lines<P4, P5, P6, P7>>,
+) -> u8
+where
+    E: DisplayHardwareLayer,
+    P4: DisplayHardwareLayer,
+    P5: DisplayHardwareLayer,
+    P6: DisplayHardwareLayer,
+    P7: DisplayHardwareLayer,
+{
+    let mut result = 0u8;
+
+    // D::delay_ns(D::ADDRESS_SETUP_TIME);
+    pins.enable.set_level(Level::High);
+
+    result |= pins.data.data7.get_value() << 3;
+    result |= pins.data.data6.get_value() << 2;
+    result |= pins.data.data5.get_value() << 1;
+    result |= pins.data.data4.get_value();
+
+    // D::delay_ns(D::ENABLE_PULSE_WIDTH);
+    pins.enable.set_level(Level::Low);
+    // D::delay_ns(D::DATA_HOLD_TIME);
+
+    result
+}
+
 /// A HD44780 compliant display.
 ///
 /// It provides a high-level and hardware agnostic interface to controll a HD44780 compliant
@@ -406,7 +484,7 @@ where
 
 impl<P, U> Display<P, U>
 where
-    P: Send + Init,
+    P: Init + Send + Recieve,
     U: Into<Address> + Home,
 {
     const FIRST_4BIT_INIT_INSTRUCTION: WriteMode = WriteMode::Command(0x33);
@@ -535,66 +613,22 @@ where
         self.pins.send(WriteMode::Data(c));
     }
 
-    // fn read_single_nibble(&self) -> u8 {
-    //     let mut result = 0u8;
-    //
-    //     D::delay_ns(D::ADDRESS_SETUP_TIME);
-    //     self.pins.enable.set_level(Level::High);
-    //
-    //     result |= self.pins.data7.get_value() << 3;
-    //     result |= self.pins.data6.get_value() << 2;
-    //     result |= self.pins.data5.get_value() << 1;
-    //     result |= self.pins.data4.get_value();
-    //
-    //     D::delay_ns(D::ENABLE_PULSE_WIDTH);
-    //     self.pins.enable.set_level(Level::Low);
-    //     D::delay_ns(D::DATA_HOLD_TIME);
-    //
-    //     result
-    // }
-    //
-    // fn receive_byte(&self) -> u8 {
-    //     let upper = self.read_single_nibble();
-    //     let lower = self.read_single_nibble();
-    //
-    //     let mut result = upper << 4;
-    //     result |= lower & 0x0f;
-    //
-    //     result
-    // }
-    //
-    // fn read_raw_byte(&self, mode: ReadMode) -> u8 {
-    //     self.pins.data4.set_direction(Direction::In);
-    //     self.pins.data5.set_direction(Direction::In);
-    //     self.pins.data6.set_direction(Direction::In);
-    //     self.pins.data7.set_direction(Direction::In);
-    //
-    //     self.pins.read.set_level(Level::High);
-    //
-    //     match mode {
-    //         ReadMode::Data => self.pins.register_select.set_level(Level::High),
-    //         ReadMode::BusyFlag => self.pins.register_select.set_level(Level::Low),
-    //     };
-    //
-    //     self.receive_byte()
-    // }
-    //
-    // /// Reads a single byte from data RAM.
-    // pub fn read_byte(&mut self) -> u8 {
-    //     self.cursor_address += Address::from(1);
-    //     self.read_raw_byte(ReadMode::Data)
-    // }
+    /// Reads a single byte from data RAM.
+    pub fn read_byte(&mut self) -> u8 {
+        self.cursor_address += Address::from(1);
+        self.pins.recieve(ReadMode::Data)
+    }
 
     /// Reads busy flag and the cursor's current address.
-    // pub fn read_busy_flag(&self) -> (bool, u8) {
-    //     let byte = self.read_raw_byte(ReadMode::BusyFlag);
-    //
-    //     let busy_flag = (byte & 0b1000_0000) != 0;
-    //
-    //     let address = byte & 0b0111_1111;
-    //
-    //     (busy_flag, address)
-    // }
+    pub fn read_busy_flag(&self) -> (bool, u8) {
+        let byte = self.pins.recieve(ReadMode::BusyFlag);
+
+        let busy_flag = (byte & 0b1000_0000) != 0;
+
+        let address = byte & 0b0111_1111;
+
+        (busy_flag, address)
+    }
 
     /// Writes the given message to data or character generator RAM, depending on the previous
     /// seek operation.
